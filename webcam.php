b9942db812d1142d41344cf561e2d34b0b2cb494
Mary <maryx@sas.upenn.edu>
2013-09-07 17:57:05 +0100

    cams hardcoded can switch places

--- webcam.php
+++ webcam.php
@@ -23,53 +23,50 @@
 </head>
 
 <body>
-<div class='container' align='center'>
-<div class='doctor' id='maincam'>
-  <div class='webcam'>
-    <img src='pic1.jpg' width='100%'>
-    <ul>
-      <li>Name:</li>
-      <li>Location:</li>
-      <li>Specialist:</li>
-      <li>Local Time:</li>
-    </ul>
+  <div class='container'>
+    <div class='cam'>
+      <div class='doctor'>
+	<div class='maincam' id='maincam'>
+	  <img src='pic1.jpg' width='100%'>
+	</div>
+      </div>
+      
+      <div class='doctor'>
+	<div class='smallcam' id='smallcam'>
+	  <img src='pic2.jpg' width='100%'> <!--100% of the div, which is 45% of box-->
+	</div>
+	
+	<div class='smallcam' id='smallcam2'>
+	  <img src='pic2.jpg' width='100%'>
+	</div>
+	
+	<div class='smallcam' id='smallcam3'>
+	  <img src='pic2.jpg' width='100%'>
+	</div>
+	
+	<div class='smallcam' id='smallcam4'>
+	  <img src='pic2.jpg' width='100%'>
+	</div>
+      </div><!--doctor-->
+    </div>
+      
+    <div class='info' align='left'>
+      <ul>
+	<li>Name:</li>
+	<li>Location:</li>
+	<li>Specialist:</li>
+	<li>Local Time:</li>
+      </ul>
+      
+      <div class='button'>
+	<button onclick='changeCams("maincam", "smallcam")'>Switch Screens</button>
+      </div>
+      
+      <div class='chat' align='left'>
+	<p> Person 1: <BR> Person 2:</p>
+      </div>
+      
+      </div>
   </div>
-  <!--div class='info' align='left'>
-    <ul>
-      <li>Name:</li>
-      <li>Location:</li>
-      <li>Specialist:</li>
-      <li>Local Time:</li>
-    </ul>
-  </div-->
-</div>
-
-<div class='doctor'>
-  <div class='smallcam' id='smallcam'>
-    <img src='pic2.jpg' width='50%'>
-  </div><!--webcam-->
-
-  <div class='smallcam' id='smallcam2'>
-    <img src='pic2.jpg' width='50%'>
-  </div><!--webcam-->
-
-  <div class='smallcam' id='smallcam3'>
-    <img src='pic2.jpg' width='50%'>
-  </div><!--webcam-->
-
-  <div class='smallcam' id='smallcam4'>
-    <img src='pic2.jpg' width='50%'>
-  </div><!--webcam-->
-</div><!--doctor-->
-
-<div class='button'>
-<button onclick='changeCams("maincam", "smallcam")'>Switch Screens</button>
-</div>
-
-<div class='chat' align='left'>
-  <p> Person 1: <BR> Person 2:</p>
-</div>
-
-</div>
 </body>
 </html>
