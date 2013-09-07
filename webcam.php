c6260f45e1c0998630424e89efa247187f4c6fd1
= <=>
2013-09-07 19:14:55 -0400

    asdasd

--- webcam.php
+++ webcam.php
@@ -1,24 +1,17 @@
 <!DOCTYPE HTML>
 <?php include_once 'head.php' ?>
-<script>
-  function changeCams(cam1, cam2){
-   
-/*   var c1 = document.getElementById(cam1); //Gets the entire div
-    var c2 = document.getElementById(cam2); //Gets the entire div
-   console.log(c1);
-   console.log(c2);
-   console.log(cam1);
-   console.log(cam2);
- */
-   var c3;
-   c3 = document.getElementById(cam1).innerHTML;
-   document.getElementById(cam1).innerHTML = document.getElementById(cam2).innerHTML;
-   document.getElementById(cam2).innerHTML = c3;
-  }  
+<script type='text/javascript'>
+	function changeCams(cam1, cam2){
+		var c3;
+		c3 = document.getElementById(cam1).innerHTML;
+		document.getElementById(cam1).innerHTML = document.getElementById(cam2).innerHTML;
+		document.getElementById(cam2).innerHTML = c3;
+	}  
 </script>
 
-	<div id='presenceDiv'></div>
-	<script type='text/javascript'>
+<div id='presenceDiv'></div>
+
+<script type='text/javascript'>
 	//Code source: https://www.firebase.com/tutorial/#session/bkb5rgke1v5
 	
 	// Prompt the user for a name to use.
@@ -99,50 +92,44 @@
 
 <div class='container'>
 	<div class='container' align='center'>
-    <div class='cam'>
-		<div class='doctor'>
-			<div class='maincam' id='maincam'>
-				<img src='pic1.jpg' width='100%'>
+		<div class='cam'>
+			<div class='doctor'>
+				<div class='maincam' id='maincam'>
+					<img src='pic1.jpg' width='100%'>
+				</div>
+			</div><!--doctor-->
+			<div class='doctor'>
+				<div class='smallcam' id='smallcam' onclick='changeCams("maincam", "smallcam")'>
+					<img src='pic2.jpg' width='100%'> <!--100% of the div, which is 45% of box-->
+				</div>
+				<div class='smallcam' id='smallcam2' onclick='changeCams("maincam", "smallcam2")'>
+					<img src='pic1.jpg' width='100%'>
+				</div>
+				<div class='smallcam' id='smallcam3' onclick='changeCams("maincam", "smallcam3")'>
+					<img src='pic2.jpg' width='100%'>
+				</div>
+				<div class='smallcam' id='smallcam4' onclick='changeCams("maincam", "smallcam4")'>
+					<img src='pic2.jpg' width='100%'>
+				</div>
+				<div class='smallcam' id='smallcam5' onclick='changeCams("maincam", "smallcam4")'>
+					<img src='pic2.jpg' width='100%'>
+				</div>
+			</div><!--doctor-->
+		</div>
+      
+		<div class='info' align='left'>
+			<h1>Information</h1>
+			<ul>
+				<li>Name:</li>
+				<li>Location:</li>
+				<li>Specialist:</li>
+				<li>Local Time:</li>
+			</ul>
+				
+			<div class='chat' align='left'>
+				<h1>Chat</h1>
+				<p> Person 1: <br /> Person 2:</p>
 			</div>
 		</div>
-    <div class='doctor'>
-	<div class='smallcam' id='smallcam' onclick='changeCams("maincam", "smallcam")'>
-	  <img src='pic2.jpg' width='100%'> <!--100% of the div, which is 45% of box-->
 	</div>
-	
-	<div class='smallcam' id='smallcam2' onclick='changeCams("maincam", "smallcam2")'>
-	  <img src='pic1.jpg' width='100%'>
-	</div>
-	
-	<div class='smallcam' id='smallcam3' onclick='changeCams("maincam", "smallcam3")'>
-	  <img src='pic2.jpg' width='100%'>
-	</div>
-	
-	<div class='smallcam' id='smallcam4' onclick='changeCams("maincam", "smallcam4")'>
-	  <img src='pic2.jpg' width='100%'>
-	</div>
- 
-	<div class='smallcam' id='smallcam5' onclick='changeCams("maincam", "smallcam4")'>
-	  <img src='pic2.jpg' width='100%'>
-	</div>
-
-      </div><!--doctor-->
-    </div>
-      
-    <div class='info' align='left'>
-      <h1>Information</h1>
-      <ul>
-	<li>Name:</li>
-	<li>Location:</li>
-	<li>Specialist:</li>
-	<li>Local Time:</li>
-      </ul>
-            
-      <div class='chat' align='left'>
-	<h1>Chat</h1>
-	<p> Person 1: <BR> Person 2:</p>
-      </div>
-      
-      </div>
-  </div>
 <?php include_once 'footer.php'; ?>
