a4f05bb90ddb6a58f09183aec4794d4b03128099
Mary <maryx@sas.upenn.edu>
2013-09-07 14:56:31 -0400

    fixed layout webcam

--- webcam.php
+++ webcam.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML>
 <?php/*php include 'head.php' */?>
 <head>
+<img src='logo.png' height='100px'>
 <link rel="stylesheet" type="text/css" href="styles.css" media="screen" />
 
 <script>
@@ -22,8 +23,8 @@
 
 </head>
 
-<body>
-  <div class='container'>
+<body align='center'>
+  <div class='container' align='center'>
     <div class='cam'>
       <div class='doctor'>
 	<div class='maincam' id='maincam'>
@@ -37,7 +38,7 @@
 	</div>
 	
 	<div class='smallcam' id='smallcam2' onclick='changeCams("maincam", "smallcam2")'>
-	  <img src='pic2.jpg' width='100%'>
+	  <img src='pic1.jpg' width='100%'>
 	</div>
 	
 	<div class='smallcam' id='smallcam3' onclick='changeCams("maincam", "smallcam3")'>
@@ -47,22 +48,25 @@
 	<div class='smallcam' id='smallcam4' onclick='changeCams("maincam", "smallcam4")'>
 	  <img src='pic2.jpg' width='100%'>
 	</div>
+ 
+	<div class='smallcam' id='smallcam5' onclick='changeCams("maincam", "smallcam4")'>
+	  <img src='pic2.jpg' width='100%'>
+	</div>
+
       </div><!--doctor-->
     </div>
       
     <div class='info' align='left'>
+      <h1>Information</h1>
       <ul>
 	<li>Name:</li>
 	<li>Location:</li>
 	<li>Specialist:</li>
 	<li>Local Time:</li>
       </ul>
-      
-      <div class='button'>
-	<button onclick='changeCams("maincam", "smallcam")'>Switch Screens</button>
-      </div>
-      
+            
       <div class='chat' align='left'>
+	<h1>Chat</h1>
 	<p> Person 1: <BR> Person 2:</p>
       </div>
       
