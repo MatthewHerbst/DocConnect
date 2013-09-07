4ead398220e25cff977bb2a2ad8744563bd1f4ad
Mary <maryx@sas.upenn.edu>
2013-09-07 23:46:48 +0100

    looks more stylish

--- head.php
+++ head.php
@@ -39,16 +39,21 @@ echo "<title>MedConnect - $userstr</title>
 	<link rel = 'stylesheet' href = 'styles.css' type = 'text/css' />
 	</head><body>
 	<div id='header'><img src='logo.png' height='100px'>
-<div style='padding:5px'>
-          <form class='form-group' align=right' class='masthead' name='search' action='search.php' method='get'>
-	      Search: <input type='text' name='fields'>
-	      <input type='submit' value='Submit'></form></a></div></div>
+        </div>
+<div>
+	  <form class='navbar-form navbar-right'>
+            <div class='form-group'>
+              <input type='text' placeholder='Search' name='fields' class='form-control'>
+            </div>
+            <button type='submit' class='btn btn-success'>Search</button>
+          </form>
+</div>
+
       <div class='masthead'>
         <ul class='nav nav-justified'>
           <li class='active'><a href='index.php'>Home</a></li>
           <li><a href='about.php'>About</a></li>
           <li><a href='leaderboard.php'>Leaderboard</a></li>
-          <li><a href='contact.php'>Contact</a></li>
 	  <li><a href='logout.php'>You are logged in as $userstr. Logout?</a></li>
         </ul>
       </div>";
