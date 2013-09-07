3a04ce1cb8ce7d262e745a652693e7393626cda3
adityans <adityans@umich.edu>
2013-09-07 01:01:00 -0400

    updated createAccount and index.php

--- createAccount.php
+++ createAccount.php
@@ -9,7 +9,23 @@ if($logInStatus)
 }
 
 echo <<<_END
+<form method = 'post' action = 'saveAccount.php'>
+<span class = 'fieldname'>First Name</span>
+<input type = 'text' maxlength = '20' name = 'firstname' /><br />
+<span class = 'fieldname'>Last Name</span>
+<input type = 'text' maxlength = '20' name = 'lastname' /><br />
+<span class = 'fieldname'>Username</span>
+<input type = 'text' maxlength = '16' name = 'user' /><br />
+<span class = 'fieldname'>Password</span>
+<input type = 'password' maxlength '16' name = 'pass' /><br />
+<span class = 'fieldname'>Date of Birth</span>
+<input type = 'date' name = 'DOB' /><br />
+<span class = 'fieldname'>Are you a Doctor?</span><br />
+<input type = 'radio' name = 'doctorStatus' value = 'yes' />Yes<br />
+<input type = 'radio' name = 'doctorStatus' value = 'no' />No<br />
+<input type = 'submit' value = 'Sign Up' />
+</form></div></body></html>
 
-
-
+_END
 ?>
+
