ac4cd794c69f549250880d070a84215ab350ead4
adityans <adityans@umich.edu>
2013-09-07 16:48:20 -0400

    updated head and login

--- head.php
+++ head.php
@@ -22,35 +22,6 @@ require_once 'tokbox/Opentok-PHP-SDK/OpenTokSession.php';
 require_once 'tok.php';
 
 
-$error = $enteredUsername = $enteredPass = "";
-
-if (isset($_POST['enteredUsername']))
-{
-    $enteredUsername = sanitizeString($_POST['enteredUsername']);
-    $enteredPass = sanitizeString($_POST['enteredPass']);
-
-    if($enteredUsername == "" || $enteredPass == "")
-    {
-        $error = "<span style='color:red'>Invalid Username or Password</span><br />";
-    }
-    else
-    {
-        $encryptedPass = sha1($enteredPass);
-        $query = "SELECT username, password FROM members WHERE user='$enteredUsername' AND password='$encryptedPass'";
-        $result = makeQuery($query);
-        if(mysql_num_rows($result) == 0)
-        {
-            $error = "<span style = 'color:red'>Invalid Username or Password</span><br />";
-        }
-        else
-        {
-            $_SESSION['user'] = $enteredUsername;
-            $_SESSION['pass'] = $enteredPass;
-            header("Location: http://localhost/DocConnect/index.php");
-            die("You are now logged in. Please <a href='members.php'?view=$enteredUsername>Click Here</a> To Continue");
-        }
-    }
-}
 
 $userstr = 'Guest';
 
@@ -70,24 +41,5 @@ echo "<title>$appname - $userstr</title>
 	<img src='logo.png' height='100px'>
 	<h1><div class = 'appname'>$appname - $userstr</div></h1>";
 
-
-
-if (!$logInStatus && !(($_GET['create']== 'yes')||($_GET['save']=='yes')))
-{
-	echo <<<_END
-	<h2>Please Login to Access Page</h2><br />
-	<form class = 'loginform' method = 'post' action = 'index.php'>$error
-	<span class = 'fieldname'>Username</span>
-	<input class = 'loginbox' type = 'text' maxlength = '16' name = 'enteredUsername' value = '$enteredUsername' /><br />
-	<span class = 'fieldname'>Password</span>
-	<input class = 'loginbox' type = 'password' maxlength = '16' name = 'enteredPass' value = '$enteredPass' /><br />
-	<br />
-	<span class = 'fieldname'></span>
-	<input type = 'submit' value = 'Login' /><br />
-	<a href = createAccount.php?create=yes>Or Sign Up Here!</a>
-	</form><br /></div>
-_END;
-	die();
-}
 
 ?>
