b6a1de4e0fd1183cbaa3035660dfce4963e5e78c
Mary B Xia <maryx@plus.seas.upenn.edu>
2013-09-07 01:30:31 -0400

    webcam page and styles functional

---
+++ webcam.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<?/*php include 'header.php' */?>
+<head>
+<link rel="stylesheet" type="text/css" href="styles.css" media="screen" />
+</head>
+
+<body>
+<div class='container' align='center'>
+
+<div class='doctor'>
+  <h1>Doctor 1</h1>
+  <div class='webscreen'>
+    <img src='pic.jpg' height='300px' width='400px'>
+  </div>
+  <div class='info' align='left';>
+    <ul>
+      <li>Name:</li>
+      <li>Location:</li>
+      <li>Specialist:</li>
+      <li>Local Time:</li>
+    </ul>
+    </p>	     
+  </div>
+</div>
+
+<div class='doctor'>
+  <h1>Doctor 2</h1>
+  <div class='webscreen'>
+    <img src='pic.jpg' height='300px' width='400px'>
+  </div><!--webscreen-->
+  <div class='info' align='left';>
+    <ul>
+      <li>Name:</li>
+      <li>Location:</li>
+      <li>Specialist:</li>
+      <li>Local Time:</li>
+    </ul>
+    </p>	     
+  </div>
+</div><!--doctor-->
+
+<div class='chat' align='left'>
+  <p> Person 1: <BR> Person 2:</p>
+</div>
+
+</div>
+</body>
+</html>
