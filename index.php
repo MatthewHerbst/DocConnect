a604b18eb2c669eb388aa46ae70696b93471937c
= <=>
2013-09-06 22:21:56 -0400

    changed index.html to index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang='en'>
+	<head>
+		<meta name='author' content=''>
+		<meta charset='utf-8'>
+		<title></title>
+		<script type='text/javascript' src='https://cdn.firebase.com/v0/firebase.js'></script>
+	</head>
+<body>
+	Hello
+	<script>
+		var myDataRef = new Firebase('https://x07k26bd3du.firebaseio-demo.com/');
+	</script>
+</body>
+</html>
