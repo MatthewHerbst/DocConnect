9d5b61d3ecceb4fd710131ad5b4e1121c987876c
adityans <adityans@umich.edu>
2013-09-07 00:19:26 -0400

    updated functions and head

---
+++ head.php
@@ -0,0 +1,28 @@
+<?
+
+session_start();
+echo "<!DOCTYPE html><br /><html<head>";
+
+include_once 'functionsneeded.php';
+
+
+$userstr = 'Guest';
+
+if (isset($_SESSION['user']))
+{
+	$user = $_SESSION['user'];
+	$logInStatus = TRUE;
+	$userstr = ""
+}
+
+else
+{
+	$logInStatus = FALSE;
+}
+
+echo "<title>$appname.'-'.$userstr</title>
+	<link rel = 'stylesheet' href = 'styles.css' type = 'text/css' />
+	</head><body><h1><div class = 'appname'>$appname.'-'.$userstr</div></h1>";
+
+
+
