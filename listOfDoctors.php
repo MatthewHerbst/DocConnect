809d6304ef247df84cb8e49547f55a9ad0c65c62
adityans <adityans@umich.edu>
2013-09-08 10:24:29 -0400

    add list of doctors

---
+++ listOfDoctors.php
@@ -0,0 +1,25 @@
+<?
+include_once 'head.php';
+
+$doctorsResult makeQuery("SELECT * FROM doctors");
+
+$numDoctors = mysql_num_rows($doctorsResult);
+
+echo "<h2>List Of All Doctors</h2><div class = 'list'><u1 class='doctorList'>";
+
+for($i = 0; $i < $numDoctors; ++$i)
+{
+	$doctorRow = mysql_fetch_assoc($doctorsResult);
+	$doctorUN = $doctorRow['username'];
+	echo "<li>$doctorUN"."    ".
+	"a href = 'viewProfile?userToDisplay=$doctorUN'>View Their Profile".
+	"</a></li?"
+}
+
+echo "<ul></div>";
+
+
+include_once 'footer.php';
+
+
+?>
