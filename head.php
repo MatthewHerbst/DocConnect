b3ad1d25561e3461df1902c88b46d753f3ff5a7c
Matthew Herbst <herbstmb@miamioh.edu>
2013-09-08 06:23:21 -0400

    removed styles

--- head.php
+++ head.php
@@ -8,7 +8,7 @@
 	<script type='text/javascript' src='js/tokbox/TB.min.js'></script>
 	<script type='text/javascript' src='js/jquery/jquery-1.10.2.min.js'></script>
 	<link rel='stylesheet' href='styles/bootstrap/bootstrap.css' type='text/css'/>
-	<link rel='stylesheet' href='styles/styles.css' type='text/css'/>
+	<!--<link rel='stylesheet' href='styles/styles.css' type='text/css'/>-->
 	<link rel='icon' type='image/png' href='img/favicon.png'>
 <?php
 ini_set('display_errors',1); 
