4fc51d02a2444c3a107fad2bc8a2655ff8063766
Matthew Herbst <herbstmb@miamioh.edu>
2013-09-08 08:24:58 -0400

    g

--- head.php
+++ head.php
@@ -110,8 +110,8 @@ echo
 						<input type='password' placeholder='Password' maxlength='16' name='enteredPass' class='form-control'>
 					</div>
 					<button type='submit' class='btn btn-success'>Sign in</button>
-				</form>
-				<a href='createAccount.php'><button type='submit' class='btn btn-success'>Register</button></a>";
+					<a href='createAccount.php'><button class='btn btn-success'>Register</button></a>
+				</form>";
 			} else { //They are logged in, show them profile info
 				echo
 				"<form class='navbar-form navbar-right' action='logout.php'>
