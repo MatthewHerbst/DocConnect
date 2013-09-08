8cea379a5f68f27d6e2b0946037f3e1e1708f873
Mary <maryx@sas.upenn.edu>
2013-09-08 03:06:40 -0400

    fixed menu into 1 line

--- head.php
+++ head.php
@@ -10,56 +10,22 @@
 	<link rel='stylesheet' href='styles/bootstrap/bootstrap.css' type='text/css'/>
 	<link rel='stylesheet' href='styles/styles.css' type='text/css'/>
 <?php
-session_start();
 
-//db and other functions
-require_once 'functionsneeded.php';
-
-//tokbox SDK
-require_once 'tokbox/Opentok-PHP-SDK/OpenTokSDK.php';
-require_once 'tokbox/Opentok-PHP-SDK/OpenTokArchive.php';
-require_once 'tokbox/Opentok-PHP-SDK/OpenTokSession.php';
-
-//For video communication
-require_once 'tok.php';
-
-
-
-$userstr = 'Guest';
-
-if(isset($_SESSION['user'])) {
-	$user = $_SESSION['user'];
-	$logInStatus = TRUE;
-	$userstr = "";
-} else 
-{
-	$logInStatus = FALSE;
-}
 
 
 echo "<title>MedConnect - $userstr</title>
 	</head><body>
-	<div id='header'><img src='img/logo.png' height='100px'>
-          <form method = 'post' action = 'search.php' 
-			class='navbar-form navbar-right'>
-            <div class='form-group'>
-              <input type='text' placeholder='Search' name='search' class='form-control'>
-            </div>
-            <button type='submit' class='btn btn-success'>Search</button>
-          </form>
-</div>
-
-      <div class='masthead'>
-        <ul class='nav nav-justified'>
-          <li class='active'><a href='index.php'>Home</a></li>
-          <li><a href='editProfile.php'>Edit Profile</a></li>
-          <li><a href='leaderboard.php'>Leaderboard</a></li>";
-	if($logInStatus)
-	{
-		echo "<li><a href='viewProfile.php?user=$user'>View Your Profile</a></li>
-				<li style='font-size: 1em;'>
-				<a href='logout.php'>Logout?</a></li>";
-	}  
-?>
+   <div class='header' style='width:15%'><img src='img/logo.png' style='width:100%'></div>
+   <div class='header' style='width:80%'>
+     <ul class='nav nav-justified'>
+       <li><a href='index.php'>Home</a></li>
+       <li><a href='viewProfile.php?user=$user'>Profile</a></li>
+       <li><a href='leaderboard.php'>Leaderboard</a></li>
+       <li><a href='logout.php'>Logout?</a></li>
+       <li><form style='background-color:#EDF1F2; border-color:#EDF1F2;' method = 'post' action = 'search.php'>
+             <input id='search' type='text' placeholder='Press Enter to Search' style='width:200px;' name='search' class='form-control'></form></li>
+     </ul>
+   </div>";  
+   ?>
 
 </ul></div>
